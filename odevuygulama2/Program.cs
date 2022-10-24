dea65da07c1811495225331649b81a4e347bdb78
Nidaklc044 <116462585+Nidaklc044@users.noreply.github.com>
2022-10-24 12:12:22 +0300

    mantıksal operatorler
    .

---
+++ odevuygulama2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odevuygulama2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int   x = 20;
+            int y = 13;
+            int z = 42;
+            Console.WriteLine(23 == 55 && 76 > 45 && 5 < 12);//False
+            Console.WriteLine(23>=23 && 45!=18);//True
+            Console.WriteLine(x>y && z==y && z<x);//False
+            Console.WriteLine(z>x && y<x);//True
+            Console.WriteLine(x!=z || y<=z);//True
+            Console.ReadLine();//
+        }
+    }
+}
